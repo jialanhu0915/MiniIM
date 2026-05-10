4c8931bee97104486928f642c9308c288cc8e4dc
Yan Runxin <231380130@mail.dhu.edu.cn>
2026-05-10 11:39:29 +0800

    添加项目文件。

---
+++ NetworkClient/NetworkClient.h
@@ -0,0 +1,32 @@
+﻿
+// NetworkClient.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CNetworkClientApp:
+// 有关此类的实现，请参阅 NetworkClient.cpp
+//
+
+class CNetworkClientApp : public CWinApp
+{
+public:
+	CNetworkClientApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CNetworkClientApp theApp;
